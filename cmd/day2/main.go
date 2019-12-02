4204d53b1d7604667109898a9bb1e3e9f1d8d2c0
William Perron <wperron@mediagrif.com>
2019-12-02 14:56:22 -0500

    day 2 -- part 2

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,15 +10,20 @@ import (
 )
 
 func main() {
-	handle, _ := os.Open("Intcode.txt")
+	handle, _ := os.Open("./Intcode.txt")
 	defer handle.Close()
 	scanner := bufio.NewScanner(handle)
 
 	for scanner.Scan() {
 		intcode := ToIntSlice(strings.Split(scanner.Text(), ","))
-		repaired := Repair(intcode)
-		calculated := Intcode(repaired)
-		fmt.Printf("Intcode at pos 0: %d\n", calculated[0])
+
+		res, err := Iterate(intcode)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Intcode at pos 0: %d\n", res[0])
 	}
 }
 
@@ -29,10 +35,27 @@ func ToIntSlice(x []string) ([]int) {
 	return y
 }
 
-func Repair(intcode []int) []int {
-	intcode[1] = 12
-	intcode[2] = 2
+func Repair(intcode []int, noun int, verb int) []int {
+	intcode[1] = noun
+	intcode[2] = verb
 	return intcode
+}
+
+func Iterate(intcode []int) ([]int, error) {
+	for i := 0; 1 < 100; i++ {
+		for j := 0; j < 100; j++ {
+			copied := make([]int, len(intcode))
+			copy(copied, intcode)
+			repaired := Repair(copied, i, j)
+
+			calculated := Intcode(repaired)
+			if calculated[0] == 19690720 {
+				fmt.Printf("Noun: %d\tVerb: %d\n", i, j)
+				return calculated, nil
+			}
+		}
+	}
+	return nil, errors.New("Permutation not found.")
 }
 
 func Intcode(intcode []int) []int {
