6dd1c4491ee57ae9ed54abb610e6286c6119e057
William Perron <wperron@mediagrif.com>
2019-12-03 08:00:22 -0500

    updated day 2 part 2 to use go routines

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -2,69 +2,89 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
+var TARGET int = 19690720
+
+type intTuple struct {
+	a, b int
+}
+
 func main() {
 	handle, _ := os.Open("./Intcode.txt")
 	defer handle.Close()
 	scanner := bufio.NewScanner(handle)
+	c := make(chan intTuple)
+
+	nouns := MakeRange(0, 100)
+	verbs := MakeRange(0, 100)
+	permuts := Permutations(nouns, verbs)
 
 	for scanner.Scan() {
 		intcode := ToIntSlice(strings.Split(scanner.Text(), ","))
 
-		res, err := Iterate(intcode)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		for _, p := range permuts {
+			go func(intcode []int, in intTuple) {
+				copied := make([]int, len(intcode))
+				copy(copied, intcode)
+
+				copied[1] = in.a
+				copied[2] = in.b
+
+				res := Intcode(copied)[0]
+
+				if res == TARGET {
+					c <- in
+					close(c)
+				}
+			}(intcode, p)
 		}
 
-		fmt.Printf("Intcode at pos 0: %d\n", res[0])
+		answer := <- c
+
+		fmt.Println("Answer is: ", answer)
 	}
 }
 
-func ToIntSlice(x []string) ([]int) {
-	y := make([] int, len(x))
+func MakeRange(min int, max int) ([]int) {
+	res := make([]int, max-min)
+	for i := range res {
+		res[i] = min + i
+	}
+	return res
+}
+
+func Permutations(a []int, b []int) ([]intTuple) {
+	var permuts []intTuple
+	for _, i := range a {
+		for _, j := range b {
+			permuts = append(permuts, intTuple{i, j})
+		}
+	}
+	return permuts
+}
+
+func ToIntSlice(x []string) []int {
+	y := make([]int, len(x))
 	for i := range x {
 		y[i], _ = strconv.Atoi(x[i])
 	}
 	return y
 }
 
-func Repair(intcode []int, noun int, verb int) []int {
-	intcode[1] = noun
-	intcode[2] = verb
-	return intcode
-}
-
-func Iterate(intcode []int) ([]int, error) {
-	for i := 0; 1 < 100; i++ {
-		for j := 0; j < 100; j++ {
-			copied := make([]int, len(intcode))
-			copy(copied, intcode)
-			repaired := Repair(copied, i, j)
-
-			calculated := Intcode(repaired)
-			if calculated[0] == 19690720 {
-				fmt.Printf("Noun: %d\tVerb: %d\n", i, j)
-				return calculated, nil
-			}
-		}
-	}
-	return nil, errors.New("Permutation not found.")
-}
-
 func Intcode(intcode []int) []int {
 	length := len(intcode)
 	for i := 0; i < length; i += 4 {
-		if intcode[i] == 99 { break }
+		if intcode[i] == 99 {
+			break
+		}
 
 		opcode := intcode[i]
-		operands := intcode[i+1:i+3]
+		operands := intcode[i+1 : i+3]
 		target := intcode[i+3]
 
 		switch opcode {
