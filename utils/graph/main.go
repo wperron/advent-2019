b9c1afdefd5dc6d0a4ece7c21048fa2e24452fff
William Perron <thewilliamperron@gmail.com>
2019-12-09 17:23:36 -0500

    day 6 -- part 2

--- utils/graph/main.go
+++ utils/graph/main.go
@@ -19,34 +19,47 @@ func Nodes(g Graph) []Node {
 	return g.nodes
 }
 
+func Edges(g Graph) map[Node][]Node {
+	return g.edges
+}
+
 func AddEdge(g *Graph, from, to Node) {
 	if g.edges == nil {
 		g.edges = make(map[Node][]Node)
 	}
 	if !contains(g.edges[from], to) {
 		g.edges[from] = append(g.edges[from], to)
+		g.edges[to] = append(g.edges[to], from)
 	}
 }
 
-func Traverse(g Graph, start Node) int {
-	var paths int
+func Traverse(g Graph, start, end Node) int {
+	distances := make(map[Node]int)
+	distances[start] = 0
 	queue := []Node{start}
+	visited := []Node{queue[0]}
 
-	for len(queue) > 0 {
+	for distances[end] == 0 {
 		curr := queue[0]
+		dist := distances[curr]
 		rel := g.edges[curr]
 		for _, adj := range rel {
-			paths++
+			if contains(visited, adj) {
+				continue
+			}
+			distances[adj] = dist + 1
+			visited = append(visited, adj)
 			queue = append(queue, adj)
 		}
 		queue = queue[1:]
 	}
 
-	return paths
+	return distances[end]
 }
 
 func Trace(g Graph, start, end Node) int {
 	var hops int
+	// magic maybe?
 	return hops
 }
 
